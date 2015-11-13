11effc41dc84854b7834b95f2d0469440d4ec75e
zhaopeng <peng.zpz@gmail.com>
2015-11-14 05:11:59 +0800

    ap pass test: not repeat in 3 times

--- spec/answerGeneratorSpec.js
+++ spec/answerGeneratorSpec.js
@@ -1,9 +1,15 @@
 var expect = require('chai').expect;
 var AnswerGenerator = require("../src/answerGenerator");
+var m = require('jsmockito').JsMockito;
+var j = require('jshamcrest').JsHamcrest;
+var anything = j.Matchers.anything;
+///JsHamcrest.Matchers.anything()
+//require('JsHamcrest').Integration.screwunit();
+//require('jsMockito').Integration.screwunit();
 
 describe("game: AnswerGenerator ", function () {
   it('should return 4 digits string.', function () {
-    var answerGenerator = new AnswerGenerator();
+    var answerGenerator = new AnswerGenerator(Math.random);
 
     var answer = answerGenerator.generate();
     var length = answer.length;
@@ -12,7 +18,7 @@ describe("game: AnswerGenerator ", function () {
   });
 
   it("should return a string combine of number", function () {
-    var answerGenerator = new AnswerGenerator();
+    var answerGenerator = new AnswerGenerator(Math.random);
 
     var answer = answerGenerator.generate();
     var notNumber = isNaN(Number(answer));
@@ -21,12 +27,39 @@ describe("game: AnswerGenerator ", function () {
   });
 
   it("should return no duplicated digits string", function () {
-    var answerGenerator = new AnswerGenerator();
+    var answerGenerator = new AnswerGenerator(Math.random);
 
     var answer = answerGenerator.generate();
 
     for(var i = 0; i < 4; i++) {
       expect(answer.indexOf(answer[i])).to.equal(answer.lastIndexOf(answer[i]));
     }
+  });
+
+  it("should have  str", function () {
+    var answerGenerator = new AnswerGenerator(Math.random);
+
+    var results = [];
+    results.push(answerGenerator.generate());
+    results.push(answerGenerator.generate());
+    results.push(answerGenerator.generate());
+    expect(results.length).to.equal(3);
+  });
+
+  it("should no repeat str in 3 times", function () {
+
+    var random = m.mockFunction();
+    m.when(random).call(anything())
+      .thenReturn(.1, .2, .3, .4)
+      .thenReturn(.2, .3, .4, .5)
+      .thenReturn(.1, .2, .3, .4)
+      .thenReturn(.3, .4, .5, .6);
+
+    var answerGenerator = new AnswerGenerator(random);
+    var results = [];
+    results.push(answerGenerator.generate());
+    results.push(answerGenerator.generate());
+    results.push(answerGenerator.generate());
+    expect(results[2]).to.equal("3456");
   });
 });
--- src/answerGenerator.js
+++ src/answerGenerator.js
@@ -1,16 +1,31 @@
-function AnswerGenerator() {
-
+function AnswerGenerator(random) {
+  this.random = random;
+  this.historyAnswer = [];
 }
 
-AnswerGenerator.prototype.generate = function() {
+var generateOnce = function (random) {
   var answer = "";
   while(answer.length < 4) {
-    var temp = Math.floor(Math.random() * 10);
-     if ( answer.indexOf(temp) === -1) {
-       answer += temp;
-     }
+    var temp = Math.floor(random() * 10);
+    if ( answer.indexOf(temp) === -1) {
+      answer += temp;
+    }
   }
   return answer;
 };
+
+AnswerGenerator.prototype.generate = function() {
+  var random = this.random;
+  var answer = generateOnce(random);
+  while(this.historyAnswer.indexOf(answer) !== -1) {
+    answer = generateOnce(random);
+  }
+  if (this.historyAnswer.length == 2) {
+    this.historyAnswer.shift();
+  }
+  this.historyAnswer.push(answer);
+  return answer;
+};
+
 
 module.exports = AnswerGenerator;
