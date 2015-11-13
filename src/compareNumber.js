7bde2bc7f0ae902922bf4f5279a05f5942097830
zhaopeng <peng.zpz@gmail.com>
2015-11-14 02:14:19 +0800

    zp:refactory compare

--- src/compareNumber.js
+++ src/compareNumber.js
@@ -3,18 +3,15 @@ function CompareNumber(){
 
 CompareNumber.prototype.compare = function (input, answer) {
   var aCount = 0;
+  var bCount = 0;
   for (var i = 0; i < input.length; i++) {
     if (answer[i] === input[i]) {
       aCount++;
     }
-  }
-  var bCount = 0;
-  for (var i = 0; i < input.length; i++) {
     if (answer.indexOf(input[i]) !== -1) {
       bCount++;
     }
   }
-
   return aCount + "A" + (bCount - aCount) + "B";
 };
 
